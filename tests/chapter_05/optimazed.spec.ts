20d2c7fdec994d225bedd4e6267411ed33aa4494
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2025-09-25 21:50:57 +0300

    YB  Project: POM

---
+++ tests/chapter_05/optimazed.spec.ts
@@ -0,0 +1,20 @@
+import { test } from "../../src/fixture/TestFixture";
+
+test("Optimized Page Object Model Test in Playwright", async ({
+	page,
+	homePage,
+	resultPage,
+	playlistPage,
+}) => {
+	// Go to URL & Search with keywords
+	await homePage.goToURL();
+	await homePage.searchWithKeywords(`${process.env.SEARCH_KEYWORDS}`);
+
+	// Click on playlist
+	await resultPage.clickOnPlaylist(`${process.env.SEARCH_KEYWORDS}`);
+
+	// Validate web page title
+	await playlistPage.validatePageTitle(
+		`${process.env.SEARCH_KEYWORDS}` + "☑️ - YouTube"
+	);
+});
