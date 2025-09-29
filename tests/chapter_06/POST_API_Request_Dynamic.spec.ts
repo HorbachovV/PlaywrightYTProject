05576f26bb870dbba3d803027aebaa04b4ee2fb1
Volodymyr Horbachov <gorbacovvolodimir@gmail.com>
2025-09-29 16:22:30 +0300

    YB  Project: API Post request using faker

--- tests/chapter_06/POST_API_Request_Dynamic.spec.ts
+++ tests/chapter_06/POST_API_Request_Dynamic.spec.ts
@@ -3,11 +3,13 @@ import { formatAPIRequest } from '../../src/utils/APIHelper';
 import path from 'path';
 import fs from 'fs';
 
+import { faker } from '@faker-js/faker';
+
 test.use({
     baseURL: process.env.BASE_API_URL,
 })
 
-test('Create POST API Request using dynamic api request body in playwright & typescript', async ({ request }) => {
+test('Create POST API Request Test using dynamic api request body', async ({ request }) => {
 
     // Reading json file
     const filePath = path.join(__dirname, '../../test-data/api_requests/Dynamic_POST_API_Request.json');
@@ -46,3 +48,45 @@ test('Create POST API Request using dynamic api request body in playwright & typ
     expect(jsonPOSTAPIResponse.booking.bookingdates.checkout).toBe('2025-01-17');
 });
 
+test('Create POST API Request Test using faker', async ({ request }) => {
+
+    // Reading json file
+    const filePath = path.join(__dirname, '../../test-data/api_requests/Dynamic_POST_API_Request.json');
+    const jsonTemplate = fs.readFileSync(filePath, 'utf-8');
+
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const totalPrice = faker.number.int({ min: 1000, max: 10000 });
+
+    const values = [firstName, lastName, totalPrice];
+
+    // Updating POST API request body
+    const postAPIRequest = await formatAPIRequest(jsonTemplate, values);
+
+    // Create POST API Request
+    const postAPIResponse = await request.post(`/booking`, { data: JSON.parse(postAPIRequest) });
+
+    // Print JSON API response
+    const jsonPOSTAPIResponse = await postAPIResponse.json();
+    console.log('POST API Response : ' + JSON.stringify(jsonPOSTAPIResponse, null, 2));
+
+    // Validating api response
+    expect(postAPIResponse.status()).toBe(200);
+    expect(postAPIResponse.statusText()).toBe('OK');
+    expect(postAPIResponse.headers()['content-type']).toContain('application/json');
+
+    // Validate propert/key names
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('firstname');
+    expect(jsonPOSTAPIResponse.booking).toHaveProperty('lastname');
+
+    expect(jsonPOSTAPIResponse.booking.bookingdates).toHaveProperty('checkin');
+    expect(jsonPOSTAPIResponse.booking.bookingdates).toHaveProperty('checkout');
+
+    // Validate API response body
+    expect(jsonPOSTAPIResponse.bookingid).toBeGreaterThan(0);
+    expect(jsonPOSTAPIResponse.booking.firstname).toBe(firstName);
+    expect(jsonPOSTAPIResponse.booking.lastname).toBe(lastName);
+
+    expect(jsonPOSTAPIResponse.booking.bookingdates.checkin).toBe('2025-01-15');
+    expect(jsonPOSTAPIResponse.booking.bookingdates.checkout).toBe('2025-01-17');
+});
